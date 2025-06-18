4bd530905173af996f8f03a142dfe6653034193b
gabiro1 <gabirojfleuron@gmail.com>
2025-06-18 15:01:59 +0200

    Completed Timestamp Microservice

--- index.js
+++ index.js
@@ -5,28 +5,53 @@
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  
+
+// serve static files
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// serve the index page
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
+// simple test endpoint
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+// Timestamp Microservice Endpoint
+app.get("/api/:date?", function (req, res) {
+  let dateInput = req.params.date;
+  let date;
+
+  // If no date parameter is provided, use current date
+  if (!dateInput) {
+    date = new Date();
+  } else {
+
+    if (!isNaN(dateInput)) {
+      date = new Date(parseInt(dateInput));
+    } else {
+      date = new Date(dateInput);
+    }
+  }
+
+  // Check if the date is valid
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
 
 
-// Listen on port set in environment variable or default to 3000
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+// start server
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Your app is listening on port http://localhost:' + listener.address().port);
 });
